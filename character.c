79c428fb02f1f8972faaccfa72696ed7a7b81d08
JaimeRetuit <jaimeromerotablado@gmail.com>
2025-02-24 19:15:01 +0100

    Módulo character

--- character.c
+++ character.c
@@ -1,3 +1,15 @@
+/**
+ * @brief It implements the functions needed to create a new character 
+ * @file character.c
+ * @author Jaime Romero
+ * 
+ * @version 0.1
+ * @date 2025-02-24
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,10 +18,121 @@
 
 struct _Character
 {
-    Id id;/*!<    */
-    char name[WORD_SIZE];/*!<    */
-    char gdesc[G_DESC];/*!<    */
-    long health;/*!<    */
-    Bool friendly;/*!<    */
-    char message[MAX_MESSAGE];/*!<    */
+    Id id;/*!< The identification for the character*/
+    char name[WORD_SIZE];/*!<Character's name*/
+    char gdesc[G_DESC];/*!<Character's description*/
+    long health;/*!<character's healthpoints*/
+    Bool friendly;/*!<Boolean value indicating if the character is friendly or not*/
+    char message[MAX_MESSAGE];/*!<Message delivered by the Character*/
 };
+
+/*============================Init============================*/
+Character* character_create(Id id){
+
+    Character *character=NULL;
+    
+    if(id==NO_ID) return NULL;
+
+    character=(Character*)malloc(sizeof(Character));
+    if(!character) return NULL;
+
+    /*Initialzation of a new character*/
+    character->id = id;
+    character->name[0]= '\0';
+    character->gdesc[0] = '\0';
+    character->health = 0;
+    character->friendly= TRUE; /*Initialized to True, it will be changed later*/
+    character->message[0]='\0';
+
+    return character;
+}
+
+Status character_destroy (Character* character){
+
+    if(!character) return ERROR;
+
+    free(character);
+    return OK;
+}
+
+/*============================Get============================*/
+const char* character_get_name(Character* character){
+    if(!character) return NO_ID;
+    return character->name;
+}
+
+const char* character_get_gdesc(Character* character){
+    if(!character) return NO_ID;
+    return character->gdesc;
+}
+
+const char* character_get_message(Character* character){
+    if(!character) return NO_ID;
+    return character->message;
+}
+
+int character_get_health(Character* character){
+    if(!character) return NO_ID;
+    return character->health;
+}
+
+Bool character_get_friendly(Character *character){
+    if(!character) return NO_ID;
+    return character->friendly;
+}
+
+/*============================Set============================*/
+Status character_set_name(Character* character, const char* name){
+    if (!character || !name) return ERROR;
+
+    if (!(strcpy(character->name, name)))
+    {
+        return ERROR;
+    }
+    return OK;
+}
+
+Status character_set_gdesc(Character* character, const char* gdesc){
+    if (!character || !gdesc) return ERROR;
+
+    if (!(strcpy(character->gdesc, gdesc)))
+    {
+        return ERROR;
+    }
+    return OK;
+}
+
+Status character_set_health(Character* character, int health){
+    if (!character || health < 0) return ERROR;
+
+    character->health = health;
+
+    return OK;
+}
+
+Status character_set_friendly(Character* character, Bool friendly){
+    if (!character) return ERROR;
+
+    character->friendly = friendly;
+    return OK;
+}
+
+Status character_set_message(Character* character, const char* message){
+    if (!character || !message) return ERROR;
+
+    if (!(strcpy(character->message, message)))
+    {
+        return ERROR;
+    }
+    return OK;    
+}
+
+/*============================Print============================*/
+Status character_print (Character* character){
+    if (!character) return ERROR;
+
+    fprintf(stdout, " Character Id: %ld\n Name: %s\n Description: %s\n", character->id, character->name, character->gdesc);
+    fprintf(stdout, " Health: %ld\n Friendly: %s\n Message: %s\n", character->health, character->friendly, character->message);
+
+    return OK;
+}
--- character.h
+++ character.h
@@ -1,3 +1,15 @@
+/**
+ * @brief It defines the functions used to create a new character 
+ * @file character.h
+ * @author Jaime Romero
+ * 
+ * @version 0.1
+ * @date 2025-02-24
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
 #ifndef CHARACTER_H
 #define CHARACTER_H
 
@@ -8,8 +20,143 @@
 
 typedef struct _Character Character;
 
+
+/*============================Init============================*/
+
+/**
+ * @brief Creates a new character from a certain Id given
+ * @author Jaime Romero
+ *
+ * @param id Character identifier
+ * 
+ * @return A new character, initiaized
+ */
 Character* character_create(Id id);
 
+/**
+ * @brief It frees the memory stored for Character
+ * @author Jaime Romero
+ * 
+ * @param character A pointer to Character
+ * @return OK if everything goes well, ERROR if anything goes wrong
+ */
+Status character_destroy (Character* character);
+
+/*============================Get============================*/
+
+/**
+ * @brief Gets the character's name
+ * @author Jaime Romero
+ *
+ * @param character a pointer to Character
+ * @return a string with the name of the character
+ */
+const char* character_get_name(Character* character);
+
+/**
+ * @brief Gets the character's description
+ * @author Jaime Romero
+ *
+ * @param character a pointer to Character
+ * @return a string with the description of the character
+ */
+const char* character_get_gdesc(Character* character);
+
+/**
+ * @brief Gets the character's message
+ * @author Jaime Romero
+ *
+ * @param character a pointer to Character
+ * @return a string with the message the character says
+ */
+const char* character_get_message(Character* character);
+
+/**
+ * @brief Reads the character's health
+ * @author Jaime Romero
+ *
+ * @param character a pointer to Character
+ * @return An integrer with the character's health
+ */
+int character_get_health(Character* character);
+
+/**
+ * @brief Gets if the character is friendly or not
+ * @author Jaime Romero
+ *
+ * @param character a pointer to the character
+ * @return TRUE if the character is friendly or FALSE if not
+ */
+Bool character_is_friendly(Character* character);
+
+/*============================Set============================*/
+
+/**
+ * @brief Assigns a name to the character
+ * @author Jaime Romero
+ *
+ * @param character a pointer to Character
+ * @param name Character's name
+ * 
+ * @return OK if everything goes well, ERROR if anything goes wrong
+ */
+Status character_set_name(Character* character, const char* name);
+
+/**
+ * @brief Defines the character's description
+ * @author Jaime Romero
+ *
+ * @param character a pointer to the character
+ * @param gdesc Character's description
+ * 
+ * @return OK if everything goes well, ERROR if anything goes wrong
+ */
+Status character_set_gdesc(Character* character, const char* gdesc);
+
+/**
+ * @brief Establishes the character's hp
+ * @author Jaime Romero
+ *
+ * @param character a pointer to the character
+ * @param health Character's healthpoints
+ * 
+ * @return OK if everything goes well, ERROR if anything goes wrong
+ */
+Status character_set_health(Character* character, int health);
+
+/**
+ * @brief Sets if the character's character (friendly or not)
+ * @author Jaime Romero
+ *
+ * @param character a pointer to the character
+ * @param friendly A boolean value indicating if the character is friendly or not
+ * 
+ * @return OK if everything goes well, ERROR if anything goes wrong
+ */
+Status character_set_friendly(Character* character, Bool friendly);
+
+/**
+ * @brief Sets the character's message
+ * @author Jaime Romero
+ *
+ * @param character a pointer to the character
+ * @param message The message given by the character
+ * 
+ * @return OK if everything goes well, ERROR if anything goes wrong
+ */
+Status character_set_message(Character* character, const char* message);
+
+/*============================Print============================*/
+
+/**
+ * @brief Prints the character's information
+ * The function displays the character's information by screen
+ * @author Jaime Romero
+ * 
+ * @param character A pointer to the character
+ * @return OK if everything goes well, ERROR if anything goes wrong
+ */
+Status character_print (Character* character);
 
 
 
